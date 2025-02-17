08d2b6fff3650922d281d91dae5f5f05bcc37426
inscrow <rickyrie98ma@gmail.com>
2025-02-17 10:27:20 +0100

    more minimalistic style
    i like it more on mobile, where the previous version was too clumsy and too full.
    
    I still need to work on the desktop version and add some small refinings, but for the most the website is complete

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
-function toggleMenu() {
-    document.querySelector('.navbar-menu').classList.toggle('active');
+function toggleNav() {
+    var nav = document.querySelector('nav');
+    nav.classList.toggle('active');
 }
+
+// fa scomparire di nuovo la barra di navigazione quando si clicca un link su mobile
+const navLinks = document.querySelectorAll('nav a');
+navLinks.forEach(link => {
+    link.addEventListener('click', function() {
+        const nav = document.querySelector('nav');
+        nav.classList.remove('active');
+    })
+})
+
+function isMobile() {
+    return /Mobi|Android/i.test(navigator.userAgent);
+}
+
+// cambia il link della posizione in base al dispositivo
+document.addEventListener('DOMContentLoaded', function() {
+    const addressLink = document.getElementById('address-link');
+    const queryString = "McDonald's+Via+Giordano+Bruno,+Alessandria";
+    if (isMobile()) {
+        addressLink.setAttribute('href', 'geo:0,0?q='+queryString);
+    } else {
+        addressLink.setAttribute('href', 'https://www.google.com/maps?q='+queryString);
+    }
+});
